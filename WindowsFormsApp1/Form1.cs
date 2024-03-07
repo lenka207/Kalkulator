f226a41ded77bce795912dde2d8bc3f8d92eff78
lenkapetkovic7@gmail.com <lenkapetkovic7@gmail.com>
2024-03-07 23:58:24 +0100

    pr6

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1169,6 +1169,7 @@ public Form1()
                 }
                 else 
                 {
+
                     int i, j;
                     string ispis1 = "";
                     int[] rez = new int[260];
